44211af922905c6ab8a3e4ea62360da7b1b4c789
Utkarsh Trivedi <utkarshtrivedi.lmp@gmail.com>
2025-09-27 14:58:26 +0530

    remaining files

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -6,24 +6,34 @@ import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user_route.js";
 import chatRoutes from "./routes/chat_route.js";
 import cors from "cors";
+import path from "path"
 
 dotenv.config(); //we always have to call this before using process.env
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json()); 
 app.use(cookieParser());
 app.use(cors({
     origin:"http://localhost:5173",
     credentials : true  //this will allow frontend to send cookie
+
  }))
 
-app.use("/auth",authConnect);
-app.use("/users",userRoutes);
-app.use("/chats",chatRoutes);
+app.use("/api/auth",authConnect);
+app.use("/api/users",userRoutes);
+app.use("/api/chats",chatRoutes);
 
+if(process.env.NODE_ENV === "production"){
+     app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     dbConnect();
--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
+
 import axios from "axios";
 
-export const axiosInstance = axios.create(
-{
-    baseURL : "http://localhost:5002",
-    withCredentials:true //send cookie with the req
-} 
-)
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api";
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true, // send cookies with the request
+});
