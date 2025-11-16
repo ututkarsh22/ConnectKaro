44ecc484caa3ca8fcf96e7eee5b1211de8d25fb6
Utkarsh Trivedi <utkarshtrivedi.lmp@gmail.com>
2025-11-17 01:03:59 +0530

    editing CORS

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -18,7 +18,7 @@ const __dirname = path.resolve();
 app.use(express.json()); 
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["https://connect-karo-sigma.vercel.app","http://localhost:5173"],
     credentials : true  //this will allow frontend to send cookie
 
  }))
